674a4a30f3379d049521810c96c916bbc639add6
paisadallas <44854381+paisadallas@users.noreply.github.com>
2022-03-26 19:26:35 -0400

    Navigation implemented and binding

--- app/src/main/java/com/john/joke/MainActivity.kt
+++ app/src/main/java/com/john/joke/MainActivity.kt
@@ -2,10 +2,23 @@ package com.john.joke
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.john.joke.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
