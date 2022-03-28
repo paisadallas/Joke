d5a00f1aeeb5bb08aca39e9af435702f3fe560b5
paisadallas <44854381+paisadallas@users.noreply.github.com>
2022-03-28 19:23:23 -0400

    Fix the data service

--- app/src/main/java/com/john/joke/view/CustomFragment.kt
+++ app/src/main/java/com/john/joke/view/CustomFragment.kt
@@ -6,15 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.john.joke.R
+import com.john.joke.databinding.FragmentCustomBinding
 
 class CustomFragment : Fragment() {
+
+    private val binding by lazy {
+        FragmentCustomBinding.inflate(layoutInflater)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_custom, container, false)
+
+        return binding.root
     }
 
 }
--- app/src/main/java/com/john/joke/view/RandomFragment.kt
+++ app/src/main/java/com/john/joke/view/RandomFragment.kt
@@ -1,13 +1,11 @@
 package com.john.joke.view
 
+import android.app.AlertDialog
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.john.joke.R
 import com.john.joke.databinding.FragmentRandomBinding
 import com.john.joke.model.Value
 import com.john.joke.utils.JokeState
@@ -18,33 +16,56 @@ class RandomFragment : BaseFragment() {
         FragmentRandomBinding.inflate(layoutInflater)
     }
 
+    private var message = ""
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        jokeViewModel.jokes.observe(viewLifecycleOwner){
-            when (it){
-                is JokeState.LOADING ->{
+        jokeViewModel.getAllJoke()
+        readData()
+
+        binding.btGenerate.setOnClickListener {
+            message()
+        }
+        return binding.root
+    }
+
+    fun message() {
+        jokeViewModel.getAllJoke()
+        readData()
+        AlertDialog.Builder(requireContext())
+            .setTitle("Joke")
+            .setMessage(message)
+            .setPositiveButton("ok") { dialogo, i ->
+                dialogo.cancel()
+            }
+            .create()
+            .show()
+    }
+
+    private fun readData() {
+        jokeViewModel.jokes.observe(viewLifecycleOwner) {
+            when (it) {
+                is JokeState.LOADING -> {
                     Toast.makeText(requireContext(), "loading...", Toast.LENGTH_LONG).show()
+                    binding.btGenerate.isEnabled = false
                 }
-                is JokeState.SUCCESS <*> ->{
-                //    val joke = it.joke as List<Value>
-                    Log.d("RESPONSE","6")
-                   var miObject:Value = it.joke as Value
-
-                    Log.d("RESPONSE_FRAGMENT",miObject.joke)
-
+                is JokeState.SUCCESS<*> -> {
+                    binding.btGenerate.isEnabled = true
+                    var miObject: Value = it.joke as Value
+                    message = miObject.joke
                 }
-                is JokeState.ERROR ->{
-                    Toast.makeText(requireContext(), it.error.localizedMessage, Toast.LENGTH_LONG).show()
+                is JokeState.ERROR -> {
+                    Toast.makeText(requireContext(), it.error.localizedMessage, Toast.LENGTH_LONG)
+                        .show()
                 }
             }
         }
 
-        jokeViewModel.getAllJoke()
-
-        return binding.root
     }
 
-}
+
+    }
+
